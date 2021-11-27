3b632e48ca5a55b147e597228eb5eb6283653578
Pereira Glauberio (PS/ESY2-LA) <glauberio.pereira@br.bosch.com>
2021-11-27 00:38:46 -0300

    redirect unknown route

--- FRONT/src/app/app-routing.module.ts
+++ FRONT/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { ContainerComponent } from 'src/app/container/container.component';
 import { LoginComponent } from './login/login.component';
 
 
+
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
-  { path: 'kanban', canActivate:[], component: ContainerComponent },
+  { path: 'kanban', canActivate: [], component: ContainerComponent },
+  { path: '**', redirectTo: '/login' },
 ];
 
 @NgModule({
